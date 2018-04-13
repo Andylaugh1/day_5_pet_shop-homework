06c3e6470820b64bdd2d56a6d4e42ec3ee3f4f99
Andy Laughlin <atl11384@gmail.com>
2018-04-13 11:18:41 +0100

    tests 1 to 5 successful

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,19 @@
 def pet_shop_name(pet_shop)
   pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  pet_shop[:admin][:total_cash] += amount
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  pet_shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(pet_shop)
+  pet_shop[:admin][:pets_sold]
+end
